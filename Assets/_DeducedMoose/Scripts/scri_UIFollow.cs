b41942e8bd34bc9538aa6d405245a970730d143a
GillGignac <59744236+GillGignac@users.noreply.github.com>
2020-03-28 15:29:48 -0400

    Added Some small stuff

---
+++ Assets/_DeducedMoose/Scripts/scri_UIFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scri_UIFollow : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.back, mainCamera.transform.rotation * Vector3.down);
+    }
+}
