3a085682bf5972d1706d319d760fe2c2a36414a6
GillGignac <59744236+GillGignac@users.noreply.github.com>
2020-03-29 16:39:36 -0400

    Potion respawn

---
+++ Assets/scri_SpawnPotion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scri_SpawnPotion : MonoBehaviour
+{
+    public GameObject potion;
+    private float waitTime = 15f;
+    public List<GameObject> _potions;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //Spawn new potion
+        InvokeRepeating("SpawnPotion", 0f , waitTime);
+    }
+
+    private void Update()
+    {
+        
+    }
+    //Spawn potion if this spawn point has no potions in it's list
+    void SpawnPotion()
+    {
+        if (!_potions.Contains(potion))
+        {
+            
+            Instantiate(potion, transform.position, Quaternion.identity);
+            //Add potion to list
+            _potions.Add(potion);
+        }
+    }
+}
